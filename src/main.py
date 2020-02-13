ce852f8fad83d4980fb74bec59cd41d4ed7a5a30
Ken Nagamine <7955031+nag8@users.noreply.github.com>
2020-02-13 19:48:36 +0900

    メール送信処理実装

--- src/main.py
+++ src/main.py
@@ -6,40 +6,85 @@ from selenium.webdriver.support.ui import Select
 from PIL import Image
 import sys
 
-print('start...')
+from smtplib import SMTP
+from email.mime.text import MIMEText
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
 
-driver = webdriver.Chrome()
-driver.get('https://airshift.jp/sft/dailyshift')
+def main():
+    print('start...')
 
-with open('../config/config.yml') as file:
-    config = yaml.load(file, Loader=yaml.SafeLoader)
+    with open('../config/config.yml') as file:
+        config = yaml.load(file, Loader=yaml.SafeLoader)
+    
+    getScreenShot(config)
+    monochrome(config)
+    sendGmailAttach(config)
+    print('finish!')
 
-# ログイン画面
-driver.find_element_by_name('username').send_keys(config['ID'])
-driver.find_element_by_name('password').send_keys(config['PASS'])
-driver.find_element_by_id('command').submit()
+# 画面遷移しスクリーンショットを保存
+def getScreenShot(config):
+    driver = webdriver.Chrome()
+    driver.get('https://airshift.jp/sft/dailyshift')
 
-time.sleep(1)
+    # ログイン画面
+    driver.find_element_by_name('username').send_keys(config['ID'])
+    driver.find_element_by_name('password').send_keys(config['PASS'])
+    driver.find_element_by_id('command').submit()
 
-# 拠点選択画面
-elements = driver.find_elements_by_class_name('searchTarget')
-elements[config['SITE']].click()
+    time.sleep(1)
 
-# デイリーレポート画面
-driver.get('https://airshift.jp/sft/dailyshift')
-time.sleep(2)
-select = Select(driver.find_element_by_name('filter-staff'))
-select.select_by_value('fixed')
+    # 拠点選択画面
+    elements = driver.find_elements_by_class_name('searchTarget')
+    elements[config['SITE']].click()
 
-driver.find_elements_by_class_name('root___1ZI4hW8s')[0].click()
-time.sleep(2)
-driver.save_screenshot(config['FILE'])
+    # デイリーレポート画面
+    driver.get('https://airshift.jp/sft/dailyshift')
+    time.sleep(2)
+    select = Select(driver.find_element_by_name('filter-staff'))
+    select.select_by_value('fixed')
 
-driver.quit()
+    driver.find_elements_by_class_name('root___1ZI4hW8s')[0].click()
+    time.sleep(2)
+    driver.save_screenshot(config['FILE'])
+
+    driver.quit()
 
 # 画像を白黒化
-img = Image.open(config['FILE'])
-img_gray = img.convert('L')
-img_gray.save(config['FILE'])
+def monochrome(config):
+    img = Image.open(config['FILE'])
+    img_gray = img.convert('L')
+    img_gray.save(config['FILE'])
 
-print('finish!')
+
+def sendGmailAttach(config):
+    sender, password = config['MAIL_ID'], config['MAIL_PASS']
+    host, port = 'smtp.gmail.com', 587
+
+    msg = MIMEMultipart()
+    msg['Subject'] = 'Airシフト印刷'
+    msg['From'] = sender
+    msg['To'] = config['MAIL_TO']
+    msg.attach(MIMEText('本文'))
+
+    # 添付ファイルの設定
+    attach_file = {'name': 'airshift.png', 'path': config['FILE']}
+    attachment = MIMEBase('image', 'png')
+
+    file = open(attach_file['path'], 'rb+')
+    attachment.set_payload(file.read())
+    file.close()
+
+    encoders.encode_base64(attachment)
+    attachment.add_header("Content-Disposition", "attachment", filename=attach_file['name'])
+    msg.attach(attachment)
+
+    # gmailへ接続(SMTPサーバーとして使用)
+    gmail=SMTP("smtp.gmail.com", 587)
+    gmail.starttls()
+    gmail.login(sender, password)
+    gmail.send_message(msg)
+
+if __name__ == '__main__':
+    main()
