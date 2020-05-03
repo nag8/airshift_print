d7cfea8f4e54d5926a7df17780ad59222646a9b2
Ken Nagamine <7955031+nag8@users.noreply.github.com>
2020-05-03 13:07:21 +0900

    標準出力に変更

---
+++ src/mail.py
@@ -0,0 +1,34 @@
+import yaml
+from smtplib import SMTP
+from email.mime.text import MIMEText
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+
+def sendGmailAttach(config):
+    sender, password = config['MAIL_ID'], config['MAIL_PASS']
+    host, port = 'smtp.gmail.com', 587
+
+    msg = MIMEMultipart()
+    msg['Subject'] = 'Airシフト印刷'
+    msg['From'] = sender
+    msg['To'] = config['MAIL_TO']
+    msg.attach(MIMEText('本文'))
+
+    # 添付ファイルの設定
+    attach_file = {'name': 'airshift.png', 'path': config['FILE']}
+    attachment = MIMEBase('image', 'png')
+
+    file = open(attach_file['path'], 'rb+')
+    attachment.set_payload(file.read())
+    file.close()
+
+    encoders.encode_base64(attachment)
+    attachment.add_header("Content-Disposition", "attachment", filename=attach_file['name'])
+    msg.attach(attachment)
+
+    # gmailへ接続(SMTPサーバーとして使用)
+    gmail=SMTP("smtp.gmail.com", 587)
+    gmail.starttls()
+    gmail.login(sender, password)
+    gmail.send_message(msg)
--- src/shift.py
+++ src/shift.py
@@ -11,12 +11,8 @@ import sys
 import subprocess
 import csv 
 
-
-from smtplib import SMTP
-from email.mime.text import MIMEText
-from email import encoders
-from email.mime.base import MIMEBase
-from email.mime.multipart import MIMEMultipart
+# local
+import mail
 
 def main():
     print('start...')
@@ -24,20 +20,17 @@ def main():
     with open('../config/config.yml') as file:
         config = yaml.load(file, Loader=yaml.SafeLoader)
 
-    with open('shift.csv', 'w') as f:
-        writer = csv.writer(f)
-        writer.writerow([datetime.date.today()])
-    
-    f.close()
-    
-    for i in range(3):
-        getScreenShot(config, i)
+    csvList = []
+    for i in range(1):
+        csvList.extend(getShiftData(config, i))
+        
         # printShift(config)
 
-    print('finish!')
+    for row in csvList:
+        print(row[0] + "," + row[1])
 
 # 画面遷移しスクリーンショットを保存
-def getScreenShot(config, siteRow):
+def getShiftData(config, placeId):
     
     options = webdriver.ChromeOptions()
     options.add_argument('--kiosk')
@@ -54,7 +47,7 @@ def getScreenShot(config, siteRow):
 
         # 拠点選択画面
         elements = driver.find_elements_by_class_name('searchTarget')
-        elements[siteRow].click()
+        elements[placeId].click()
 
         # デイリーレポート画面
         # 日時指定する場合は、URLを以下などに変更すること
@@ -72,19 +65,22 @@ def getScreenShot(config, siteRow):
 
         names = soup.findAll('div',class_="name___1yaaRDba")
         siteList = ["渋谷","難波","新宿"]
+        
+        csvlist = []
+        
+        for name in names:
+            csvlist.append([name.text.replace('z', '').replace('(AI)', ''),siteList[placeId]])
 
-        with open('shift.csv', 'a') as f:
-            writer = csv.writer(f)
-            for name in names:
-                writer.writerow([name.text.replace('z', '').replace('(AI)', ''),siteList[siteRow]])
-
-        f.close()
         driver.save_screenshot(config['FILE'])
+        sendSlack(config, siteList[placeId])
         print("take photo")
         time.sleep(20)
-        sendSlack(config, siteList[siteRow])
+
+        return csvlist
+        
     except Exception as e:
         print(e)
+        return [[]]
     finally:
         driver.quit()
 
@@ -98,33 +94,6 @@ def monochrome(config):
     img_gray = img.convert('L')
     img_gray.save(config['FILE'])
 
-def sendGmailAttach(config):
-    sender, password = config['MAIL_ID'], config['MAIL_PASS']
-    host, port = 'smtp.gmail.com', 587
-
-    msg = MIMEMultipart()
-    msg['Subject'] = 'Airシフト印刷'
-    msg['From'] = sender
-    msg['To'] = config['MAIL_TO']
-    msg.attach(MIMEText('本文'))
-
-    # 添付ファイルの設定
-    attach_file = {'name': 'airshift.png', 'path': config['FILE']}
-    attachment = MIMEBase('image', 'png')
-
-    file = open(attach_file['path'], 'rb+')
-    attachment.set_payload(file.read())
-    file.close()
-
-    encoders.encode_base64(attachment)
-    attachment.add_header("Content-Disposition", "attachment", filename=attach_file['name'])
-    msg.attach(attachment)
-
-    # gmailへ接続(SMTPサーバーとして使用)
-    gmail=SMTP("smtp.gmail.com", 587)
-    gmail.starttls()
-    gmail.login(sender, password)
-    gmail.send_message(msg)
 
 def sendSlack(config, fileName):
     subprocess.call(["slackcat", "--channel", config['SLACK_CHANNEL'], "--filename",fileName + ".png", config['FILE']])
