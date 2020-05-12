8b9ef53796892a8eef0095983a2190273fea2527
Ken Nagamine <7955031+nag8@users.noreply.github.com>
2020-05-12 22:57:54 +0900

    タイマー処理

---
+++ src/timer.rb
@@ -0,0 +1,18 @@
+require 'time'
+def timer(arg, &proc)
+  x = case arg
+  when Numeric then arg
+  when Time    then arg - Time.now
+  when String  then Time.parse(arg) - Time.now
+  else raise   end
+
+  sleep x if block_given?
+  yield
+end
+
+# これがないとエラーになる。いつか直す
+timer("24:00"){}
+
+timer("08:00") do
+  system("python shift.py")
+end
